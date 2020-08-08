1da0c6a7dba44dcede27f723fe38618c821e395f
Jose Henrique Roveda <zeh.henrique92@gmail.com>
2020-08-08 18:26:32 -0300

    Base project

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def read_main():
+    return {'message': 'Hello World'}
+
+
+@app.get('/article/{article_id}')
+def analyze_article(article_id: int):
+    return {'article_id': article_id}
